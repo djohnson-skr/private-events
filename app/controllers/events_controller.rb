de70d04317456ebde72f7fccffc7bf9fcf2af0e8
Armin Efendic <arminefendic12@gmail.com>
2022-02-21 10:44:54 -0500

    Ability to create an event linked with a user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,28 @@
 class EventsController < ApplicationController
   def index
-    #@events = Events.all
+    @events = Event.all
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      flash[:succes] = 'New Event Created!'
+      redirect_to events_path
+    else
+      flash[:error] = "Check your event details"
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'user/:id' => 'user#show'
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
   
   root to: "events#index"
 end
