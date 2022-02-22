79c325afb720d128f2745f97dd28aab220397784
Armin Efendic <arminefendic12@gmail.com>
2022-02-21 21:35:30 -0500

    Ability to RSVP to an event

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,3 +1,16 @@
 class RsvpsController < ApplicationController
+  def create
+    @rsvp = Rsvp.new(rsvp_params)
+
+    if @rsvp.save
+      redirect_to events_path
+    end
+  end
+
+  private
+
+  def rsvp_params
+    params.require(:rsvp).permit(:attended_event_id, :attendee_id)
+  end
 
 end
