00a892209991768d5fd000da22b0316ee00fbea4
Armin Efendic <arminefendic12@gmail.com>
2022-02-21 20:21:48 -0500

    Added missing ':'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :rsvps, foreign_key: :attendee_id, class_name: "Rsvp"
+  has_many :attended_events, through: :rsvps
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
